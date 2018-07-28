5c688fada1b6b58fbb0fed7560da333f0a6fbeac
skoh5 <skoh5@skoh5-PC>
2018-07-28 09:55:14 +0900

    소숫점 찾기 개선

--- algorithm_train/src/com/skoh5/day3/FindPrimeNum.java
+++ algorithm_train/src/com/skoh5/day3/FindPrimeNum.java
@@ -25,10 +25,32 @@ public class FindPrimeNum {
 		}
 	}
 	
+	private int solution2(int n) {
+		int answer = 0;
+		int[] arrNum = new int[n];
+		for(int i=2;i<=n;i++) {
+			if(arrNum[i-1] < 0) {
+				continue;
+			}
+			answer++;
+			setMultiple(i, n, arrNum);
+		}
+		return answer;
+	}
+	
+	private void setMultiple(int num, int maxNum, int[] arrNum) {
+		int multipleIdx = 1;
+		int findNum = num * multipleIdx++;
+ 		while(findNum <= maxNum) {
+ 			arrNum[findNum-1] = -1;
+			findNum = num * multipleIdx++;
+		}
+	}
+	
 	public static void main(String[] args) {
 		long startTime = System.currentTimeMillis();
 		//code
-		System.out.println(new FindPrimeNum().solution(Integer.parseInt(args[0])));
+		System.out.println(new FindPrimeNum().solution2(Integer.parseInt(args[0])));
 		long endTime = System.currentTimeMillis();
 		System.out.println("Took "+(endTime - startTime) + " ms");
 	}
